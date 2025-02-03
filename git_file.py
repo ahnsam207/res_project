dbd902a715dee8e7540b3e35754c566b4e20eef7
ahnsam207 <2ok25@daum.net>
2025-02-03 17:04:45 +0900

    git_file.py

--- git_file.py
+++ git_file.py
@@ -7,11 +7,20 @@ GITHUB_REPO = "ahnsam207/res_project"
 GITHUB_TOKEN =  st.secrets["git_token"]   # 개인 액세스 토큰 입력
 BRANCH = "main"  # 사용할 브랜치
 
+st.title("KB 프로젝트 수업 계획서 제출")
 
+st.sidebar.header("파/일/관/리")
 
+select_option = ["융합교과", "단일교과"]
+your_option = st.sidebar.selectbox("형태", select_option)
+user_name = st.sidebar.text_input("제출자 이름:", max_chars =15)
+st.sidebar.text("※ 대표자만 입력")
+subject = st.sidebar.text_input("교과명:", type = "password")
+st.sidebar.text("※ 융합교과는 [수학-프로그래밍] 형식으로 입력")
 
-# 파일 업로드
-uploaded_file = st.file_uploader(" 계획서 파일을 업로드하세요")
+# 메인
+uploaded_file = st.file_uploader("계획서 파일을 업로드하세요")
+uploaded_file.name = select_option + "_" + user_name + "_" + subject + "_" + uploaded_file
 
 if uploaded_file is not None:
     file_content = uploaded_file.getvalue()
